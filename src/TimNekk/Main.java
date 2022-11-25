987ce2313958f4919c235dd17b3fd50729e3a1f3
Tim <herew26@gmail.com>
2022-11-26 00:36:39 +0300

    feat: change to new classes

--- src/TimNekk/Main.java
+++ src/TimNekk/Main.java
@@ -1,20 +1,29 @@
 package TimNekk;
 
 import TimNekk.controller.ConsoleController;
-import TimNekk.model.CellState;
 import TimNekk.model.Field;
+import TimNekk.model.GameFlow;
+import TimNekk.model.GameMode;
+import TimNekk.model.NoMoreMovesException;
 import TimNekk.view.ConsoleView;
 
 public class Main {
     public static void main(String[] args) {
-        Field field = new Field(8, 8);
-        ConsoleView view = new ConsoleView(field);
-        ConsoleController controller = new ConsoleController(field, view);
+        Field field = new Field();
+        GameFlow gameFlow = new GameFlow(field, GameMode.PLAYER_VS_BOT);
+
+        ConsoleView view = new ConsoleView(gameFlow);
+        ConsoleController controller = new ConsoleController(view, gameFlow);
 
         view.update();
 
-    while (true) {
-            controller.ReadInput();
+        while (true) {
+            try {
+                controller.makeMove();
+            } catch (NoMoreMovesException e) {
+                view.showGameEnd();
+                break;
+            }
         }
     }
 }
