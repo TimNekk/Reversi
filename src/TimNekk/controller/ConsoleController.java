a82b6a148d06bd20b45731d9cf4177aba344c556
Tim <herew26@gmail.com>
2022-11-26 00:36:22 +0300

    feat: configure new model and view

--- src/TimNekk/controller/ConsoleController.java
+++ src/TimNekk/controller/ConsoleController.java
@@ -1,31 +1,44 @@
 package TimNekk.controller;
 
-import TimNekk.model.CellState;
-import TimNekk.model.Field;
+import TimNekk.model.Coordinates;
+import TimNekk.model.GameFlow;
+import TimNekk.model.NoMoreMovesException;
 import TimNekk.view.ConsoleView;
 
 import java.util.Scanner;
 
 public class ConsoleController {
     private final ConsoleView view;
-    private final Field field;
+    private final GameFlow gameFlow;
     private final Scanner scanner;
 
-    public ConsoleController(Field field, ConsoleView view) {
-        this.field = field;
+    public ConsoleController(ConsoleView view, GameFlow gameFlow) {
         this.view = view;
+        this.gameFlow = gameFlow;
         scanner = new Scanner(System.in);
     }
 
-    public void ReadInput() {
-        // read coordinates
+    public void makeMove() throws NoMoreMovesException {
+        if (gameFlow.isPlayerTurn()) {
+            Coordinates coordinates = getPlayerMove();
+            gameFlow.makePlayerMove(coordinates);
+        } else {
+            waitForEnter();
+            gameFlow.makeBotMove();
+        }
+
+        view.update();
+    }
+
+    private void waitForEnter() {
+        scanner.nextLine();
+        scanner.nextLine();
+    }
+
+    private Coordinates getPlayerMove() {
         int x = scanner.nextInt();
         int y = scanner.nextInt();
 
-        // set cell state
-        field.getCell(x, y).setState(CellState.BLACK);
-
-        // update view
-        view.update();
+        return new Coordinates(x, y);
     }
 }
