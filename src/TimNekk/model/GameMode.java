a7057ab74ec5f6733fe6fee88957e4c4ae795d83
TimNekk <herew26@gmail.com>
2022-12-02 17:39:14 +0300

    feat: add parsing method

--- src/TimNekk/model/GameMode.java
+++ src/TimNekk/model/GameMode.java
@@ -1,5 +1,15 @@
 package TimNekk.model;
 
 public enum GameMode {
-    PLAYER_VS_BOT, PLAYER_VS_ADVANCED_BOT, PLAYER_VS_PLAYER
+    PLAYER_VS_SIMPLE_BOT, PLAYER_VS_ADVANCED_BOT, PLAYER_VS_PLAYER;
+
+    public static GameMode parseInt(int number) {
+        switch (number) {
+            case 1: return PLAYER_VS_SIMPLE_BOT;
+            case 2: return PLAYER_VS_ADVANCED_BOT;
+            case 3: return PLAYER_VS_PLAYER;
+        }
+
+        throw new IllegalArgumentException("Game mode can only be converted from: 1, 2, 3");
+    }
 }
