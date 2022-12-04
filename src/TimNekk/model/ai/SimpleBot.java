c7c0e081df9500ff0c6c8d7388de359cf3517c87
Tim <herew26@gmail.com>
2022-12-04 15:06:11 +0300

    feat: add move selecting logic

--- src/TimNekk/model/ai/SimpleBot.java
+++ src/TimNekk/model/ai/SimpleBot.java
@@ -4,7 +4,7 @@ import TimNekk.model.Coordinates;
 import TimNekk.model.Field;
 import TimNekk.model.Turn;
 
-import java.util.List;
+import java.util.Set;
 
 public class SimpleBot extends Bot {
 
@@ -14,7 +14,47 @@ public class SimpleBot extends Bot {
 
     @Override
     public Coordinates getMove(Turn turn) {
-        List<Coordinates> coordinates = getAvailableCellsCoordinates(turn);
-        return coordinates.get((int) (Math.random() * coordinates.size()));
+        Set<Coordinates> coordinates = field.getAvailableCellsCoordinates(turn);
+
+        float maxRating = 0;
+
+        Coordinates bestMove = null;
+
+        for (Coordinates coordinate : coordinates) {
+            float rating = GetMoveRating(coordinate, turn);
+
+            if (rating > maxRating) {
+                maxRating = rating;
+                bestMove = coordinate;
+            }
+        }
+
+        return bestMove;
+    }
+
+    private float GetMoveRating(Coordinates coordinates, Turn turn) {
+        float rating = GetPlacedCellRating(coordinates);
+
+        Set<Coordinates> surrenderedCoordinatesSet = field.getSurrenderedCellsCoordinates(coordinates, turn);
+
+        for (Coordinates surrenderedCoordinates : surrenderedCoordinatesSet) {
+            rating += GetSurrenderedCellRating(surrenderedCoordinates);
+        }
+        
+        return rating;
+    }
+
+    private int GetSurrenderedCellRating(Coordinates surrenderedCoordinates) {
+        return field.isEdgeCell(surrenderedCoordinates) ? 2 : 1;
+    }
+    
+    private float GetPlacedCellRating(Coordinates coordinates) {
+        if (field.isCornerCell(coordinates)) {
+            return 0.8f;
+        }
+        if (field.isEdgeCell(coordinates)) {
+            return 0.4f;
+        }
+        return 0;
     }
 }
