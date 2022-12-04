644994d2dfdeee72e52f67884788a9033245a60b
Tim <herew26@gmail.com>
2022-12-04 15:04:38 +0300

    feat: make "field" protected

--- src/TimNekk/model/ai/Bot.java
+++ src/TimNekk/model/ai/Bot.java
@@ -4,17 +4,11 @@ import TimNekk.model.Coordinates;
 import TimNekk.model.Field;
 import TimNekk.model.Turn;
 
-import java.util.List;
-
 public abstract class Bot {
-    private final Field field;
+    protected final Field field;
 
     public Bot(Field field) {
         this.field = field;
-    }
-
-    protected List<Coordinates> getAvailableCellsCoordinates(Turn turn) {
-        return field.getAvailableCellsCoordinates(turn);
     }
 
     public abstract Coordinates getMove(Turn turn);
