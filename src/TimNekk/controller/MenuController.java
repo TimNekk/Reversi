fc17ee6912e11474a6b5a6e524417e8b69201c6d
Tim <herew26@gmail.com>
2022-12-04 20:20:17 +0300

    chore: add javadoc to controllers

--- src/TimNekk/controller/MenuController.java
+++ src/TimNekk/controller/MenuController.java
@@ -1,19 +1,33 @@
 package TimNekk.controller;
 
 import TimNekk.model.GameFlow;
-import TimNekk.model.GameMode;
-import TimNekk.model.MenuItem;
+import TimNekk.model.enums.GameMode;
+import TimNekk.model.enums.MenuItem;
 
 import java.util.Scanner;
 
+/**
+ * Controller for menu actions
+ */
 public class MenuController extends Controller {
     private final GameFlow gameFlow;
 
+    /**
+     * Creates a new instance of the controller.
+     *
+     * @param scanner  Scanner for user input
+     * @param gameFlow GameFlow instance
+     */
     public MenuController(Scanner scanner, GameFlow gameFlow) {
         super(scanner);
         this.gameFlow = gameFlow;
     }
 
+    /**
+     * Handles user input for menu actions
+     *
+     * @return MenuItem
+     */
     public MenuItem handlerMenuInput() {
         MenuItem menuItem = getMenuItem();
 
@@ -25,6 +39,13 @@ public class MenuController extends Controller {
         return menuItem;
     }
 
+    /**
+     * Reads an integer from the user.
+     * If the user enters an integer value that is not a valid menu item,
+     * the method will ask the user to enter a new value.
+     *
+     * @return MenuItem from user input
+     */
     private MenuItem getMenuItem() {
         while (true) {
             int menuNumber = readInteger();
--- src/TimNekk/controller/MovesController.java
+++ src/TimNekk/controller/MovesController.java
@@ -7,14 +7,27 @@ import TimNekk.model.exceptions.NoMoreMovesException;
 
 import java.util.Scanner;
 
+/**
+ * Controller for handling moves
+ */
 public class MovesController extends Controller {
     private final GameFlow gameFlow;
 
+    /**
+     * Creates a new instance of the controller.
+     *
+     * @param scanner  Scanner for user input
+     * @param gameFlow GameFlow instance
+     */
     public MovesController(Scanner scanner, GameFlow gameFlow) {
         super(scanner);
         this.gameFlow = gameFlow;
     }
 
+    /**
+     * Handles user input for moves and makes a move.
+     * If the user enters an invalid move, the method will ask the user to enter a new value.
+     */
     public void makeMove() throws NoMoreMovesException, IllegalMoveException {
         if (gameFlow.isPlayerTurn()) {
             Coordinates coordinates = getPlayerMove();
@@ -28,12 +41,23 @@ public class MovesController extends Controller {
         }
     }
 
+    /**
+     * Reads string from the user.
+     *
+     * @return true if the user entered "u" or "U", false otherwise
+     */
     private boolean isUndoInInput() {
         scanner.nextLine();
         String input = scanner.nextLine();
         return input.equalsIgnoreCase("u");
     }
 
+    /**
+     * Reads coordinates from the user.
+     * If the user enters an invalid coordinates, the method will ask the user to enter a new value.
+     *
+     * @return Coordinates from user input
+     */
     private Coordinates getPlayerMove() {
         int x = readInteger();
         int y = readInteger();
